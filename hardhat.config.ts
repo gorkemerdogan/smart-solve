f44bced1a68118cb8c38b57719076df34f2876d9
gorkemerdogan <mgorkem.erdogan@gmail.com>
2025-09-14 10:42:00 +0300

    HardHat config update for optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,12 @@ import type { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: { enabled: true, runs: 200 }
+    }
+  }
 };
 
 export default config;
