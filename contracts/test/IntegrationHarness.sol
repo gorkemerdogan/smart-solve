b603479510666cb604a559adfbfd94787e2729b7
gorkemerdogan <mgorkem.erdogan@gmail.com>
2025-11-18 09:37:58 +0300

    ABDKMathQuad -> MathLib update

--- contracts/test/IntegrationHarness.sol
+++ contracts/test/IntegrationHarness.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-import { ABDKMathQuad as Q } from "abdk-libraries-solidity/ABDKMathQuad.sol";
-import { Trigonometric } from "../libraries/Trigonometric.sol";
+import { Trigonometry } from "../libraries/Trigonometry/Trigonometry.sol";
 import { QuadConstants as QC } from "../libraries/QuadConstants.sol";
+import { MathLib } from "../libraries/MathLib.sol";
 import { Integration } from "../libraries/numeric/Integration.sol";
 
 /**
@@ -13,38 +13,38 @@ import { Integration } from "../libraries/numeric/Integration.sol";
  *         simple integrand functions.
  */
 contract IntegrationHarness {
-    using Q for bytes16;
+    using MathLib for bytes16;
 
     // ---------- Helpers: constants / conversions ----------
 
     function ZERO() external pure returns (bytes16) {
-        return Q.fromInt(0);
+        return MathLib.fromInt(0);
     }
 
     function ONE() external pure returns (bytes16) {
-        return Q.fromInt(1);
+        return MathLib.fromInt(1);
     }
 
     function TWO() external pure returns (bytes16) {
-        return Q.fromInt(2);
+        return MathLib.fromInt(2);
     }
 
     function qFromInt(int256 x) external pure returns (bytes16) {
-        return Q.fromInt(x);
+        return MathLib.fromInt(x);
     }
 
     function qFromUInt(uint256 x) external pure returns (bytes16) {
-        return Q.fromUInt(x);
+        return MathLib.fromUInt(x);
     }
 
     function qFromFrac(int256 num, int256 den) external pure returns (bytes16) {
-        bytes16 n = Q.fromInt(num);
-        bytes16 d = Q.fromInt(den);
+        bytes16 n = MathLib.fromInt(num);
+        bytes16 d = MathLib.fromInt(den);
         return n.div(d);
     }
 
     function cmp(bytes16 a, bytes16 b) external pure returns (int256) {
-        return Q.cmp(a, b);
+        return MathLib.cmp(a, b);
     }
 
     /// @notice |a - b| in quad
@@ -56,12 +56,12 @@ contract IntegrationHarness {
 
     /// f(x) = 0
     function f_zero(bytes16 /*x*/) external pure returns (bytes16) {
-        return Q.fromInt(0);
+        return MathLib.fromInt(0);
     }
 
     /// f(x) = 1
     function f_one(bytes16 /*x*/) external pure returns (bytes16) {
-        return Q.fromInt(1);
+        return MathLib.fromInt(1);
     }
 
     /// f(x) = x
@@ -86,28 +86,28 @@ contract IntegrationHarness {
 
         /// f(x) = 5
     function f_const5(bytes16 /*x*/) external pure returns (bytes16) {
-        return Q.fromInt(5);
+        return MathLib.fromInt(5);
     }
 
     /// f(x) = sin(x)
     function f_sin(bytes16 x) external pure returns (bytes16) {
-        return Trigonometric.sin(x);
+        return Trigonometry.sin(x);
     }
 
     /// f(x) = 1/x
     function f_inv(bytes16 x) external pure returns (bytes16) {
-        return Q.fromInt(1).div(x);
+        return MathLib.fromInt(1).div(x);
     }
 
     /// f(x) = 1e-30 * x
     function f_tiny(bytes16 x) external pure returns (bytes16) {
-        bytes16 scale = Q.fromUInt(1).div(Q.fromUInt(10**30));
+        bytes16 scale = MathLib.fromUInt(1).div(MathLib.fromUInt(10**30));
         return x.mul(scale);
     }
 
     /// f(x) = 1e20 * x
     function f_large(bytes16 x) external pure returns (bytes16) {
-        bytes16 scale = Q.fromUInt(10**20);
+        bytes16 scale = MathLib.fromUInt(10**20);
         return x.mul(scale);
     }
 
@@ -115,13 +115,13 @@ contract IntegrationHarness {
     /// [0,1]: f=1
     /// [1,2]: f=3
     function f_piecewise(bytes16 x) external pure returns (bytes16) {
-        bytes16 one = Q.fromInt(1);
-        bytes16 two = Q.fromInt(2);
+        bytes16 one = MathLib.fromInt(1);
+        bytes16 two = MathLib.fromInt(2);
 
-        if (Q.cmp(x, one) <= 0) {
-            return Q.fromInt(1);
-        } else if (Q.cmp(x, two) <= 0) {
-            return Q.fromInt(3);
+        if (MathLib.cmp(x, one) <= 0) {
+            return MathLib.fromInt(1);
+        } else if (MathLib.cmp(x, two) <= 0) {
+            return MathLib.fromInt(3);
         } else {
             revert("piecewise domain");
         }
