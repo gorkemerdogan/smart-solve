34503e4e1a39dc241de7d51bbbc5d8415f4540e6
gorkemerdogan <mgorkem.erdogan@gmail.com>
2025-11-19 13:29:28 +0300

    viaIR: true for stack too deep error

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,7 +5,8 @@ const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.28",
     settings: {
-      optimizer: { enabled: true, runs: 200 }
+      optimizer: { enabled: true, runs: 200 },
+      viaIR: true
     }
   }
 };
