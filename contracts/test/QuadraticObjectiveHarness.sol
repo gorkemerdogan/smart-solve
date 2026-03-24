8fa9c0dcebd73902f793ea40700b3b8fbbd4f32e
gorkemerdogan <mgorkem.erdogan@gmail.com>
2026-03-24 13:59:54 +0300

    Add: Weighted quadratic function for growth tests

--- contracts/test/QuadraticObjectiveHarness.sol
+++ contracts/test/QuadraticObjectiveHarness.sol
@@ -41,3 +41,48 @@ contract QuadraticObjectiveHarness is IObjectiveFunction {
         }
     }
 }
+
+/**
+ * @title  WeightedQuadraticObjectiveHarness
+ * @notice Test objective for generic steepest descent experiments.
+ *         Implements the weighted quadratic function
+ *         g(x) = sum_i w_i * x_i^2
+ *         with gradient
+ *         grad g(x)_i = 2 * w_i * x_i
+ */
+contract WeightedQuadraticObjectiveHarness {
+    using MathLib for bytes16;
+
+    /**
+     * @notice Evaluates the weighted quadratic objective g(x) = sum_i (i+1) * x_i^2.
+     * @param  x Input vector in quad-precision.
+     * @return value Objective value g(x) in quad-precision.
+     */
+    function g(bytes16[] memory x) external pure returns (bytes16 value) {
+        value = MathLib.fromUInt(0);
+
+        for (uint256 i = 0; i < x.length; ++i) {
+            bytes16 weight = MathLib.fromUInt(i + 1);
+            bytes16 xi2 = x[i].mul(x[i]);
+            value = value.add(weight.mul(xi2));
+        }
+    }
+
+    /**
+     * @notice  Computes the gradient of the weighted quadratic objective.
+     * @dev     For g(x) = sum_i (i+1) * x_i^2,
+     *          grad g(x)_i = 2 * (i+1) * x_i.
+     * @param x Input vector in quad-precision.
+     * @return gradVec Gradient vector in quad-precision.
+     */
+    function grad(bytes16[] memory x) external pure returns (bytes16[] memory gradVec) {
+        gradVec = new bytes16[](x.length);
+
+        bytes16 two = MathLib.fromUInt(2);
+
+        for (uint256 i = 0; i < x.length; ++i) {
+            bytes16 weight = MathLib.fromUInt(i + 1);
+            gradVec[i] = two.mul(weight).mul(x[i]);
+        }
+    }
+}
